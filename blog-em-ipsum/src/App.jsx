67eba47500be38ff07b66cf65101dc1c21ffbfe2
Viktor Strelianyi <chmpain@gmail.com>
2022-10-27 17:19:05 +0300

    - lesson 7 start

--- blog-em-ipsum/src/App.jsx
+++ blog-em-ipsum/src/App.jsx
@@ -2,6 +2,7 @@ import { Posts } from "./Posts";
 import "./App.css";
 
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const queryClient = new QueryClient();
 
@@ -14,6 +15,7 @@ function App() {
       <h1>Blog Posts</h1>
       <Posts />
     </div>
+		<ReactQueryDevtools/>
 		</QueryClientProvider>
   );
 }
--- blog-em-ipsum/src/Posts.jsx
+++ blog-em-ipsum/src/Posts.jsx
@@ -7,7 +7,7 @@ const maxPostPage = 10;
 
 async function fetchPosts() {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com /posts?_limit=10&_page=0"
+    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
   );
   return response.json();
 }
